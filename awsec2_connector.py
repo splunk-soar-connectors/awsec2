24f3614f447d61a975d5bda7d9c7487d8c4478dd
Jay Patel <jay.patel@M06-JayP.local>
2019-04-10 14:58:17 +0530

    AWSEC2: Initial commit for all the bugs fixation for the app

--- awsec2_connector.py
+++ awsec2_connector.py
@@ -80,7 +80,9 @@ class AwsEc2Connector(BaseConnector):
         try:
             resp_json = boto_func(**kwargs)
         except Exception as e:
+            self.debug_print('exception')
             return RetVal(action_result.set_status(phantom.APP_ERROR, 'boto3 call to ec2 failed', e), None)
+        self.debug_print('unreachable')
 
         return phantom.APP_SUCCESS, self._sanitize_data(resp_json)
 
@@ -250,23 +252,32 @@ class AwsEc2Connector(BaseConnector):
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
+        self.debug_print('MLA 111')
         if not self._create_client('ec2', action_result):
             return action_result.get_status()
 
+        self.debug_print('MLA 222')
         filters = param.get('filters')
         instance_ids = param.get('instance_ids')
         dry_run = param.get('dry_run')
         max_results = param.get('max_results')
         next_token = param.get('next_token')
 
+        self.debug_print('ABCD 111')
+        self.debug_print('Type is: {0}'.format(type(max_results)))
         args = dict()
         if filters:
-            evaluated_filters = list(ast.literal_eval(filters))
-            # If only one filter
-            if type(evaluated_filters[0]) == str:
-                args['Filters'] = [ast.literal_eval(filters)]
-            else:
-                args['Filters'] = evaluated_filters
+            try:
+                evaluated_filters = list(ast.literal_eval(filters))
+                # If only one filter
+                if type(evaluated_filters[0]) == str:
+                    args['Filters'] = [ast.literal_eval(filters)]
+                else:
+                    args['Filters'] = evaluated_filters
+            except Exception as e:
+                return action_result.set_status(phantom.APP_ERROR, 'Error occured while parsing filter : {0}'.format(str(e)))
+
+        self.debug_print('ABCD 222')
         if instance_ids:
             args['InstanceIds'] = [item.strip() for item in instance_ids.split(',')]
         if dry_run:
@@ -276,19 +287,24 @@ class AwsEc2Connector(BaseConnector):
         if next_token:
             args['NextToken'] = next_token
 
+        self.debug_print('ABCD 333')
         # make rest call
         ret_val, response = self._make_boto_call(action_result, 'describe_instances', **args)
 
+        self.debug_print('ABCD 444')
         if (phantom.is_fail(ret_val)):
             return action_result.get_status()
 
+        self.debug_print('ABCD 555')
         # Add the response into the data section
         action_result.add_data(response)
 
+        self.debug_print('ABCD 666')
         # Add a dictionary that is made up of the most important values from data into the summary
         summary = action_result.update_summary({})
         summary['num_instances'] = len(response.get('Reservations', {}))
 
+        self.debug_print('ABCD 777')
         return action_result.set_status(phantom.APP_SUCCESS)
 
     def _handle_detach_instance(self, param):
@@ -535,12 +551,15 @@ class AwsEc2Connector(BaseConnector):
 
         args = dict()
         if filters:
-            evaluated_filters = list(ast.literal_eval(filters))
-            # If only one filter
-            if type(evaluated_filters[0]) == str:
-                args['Filters'] = [ast.literal_eval(filters)]
-            else:
-                args['Filters'] = evaluated_filters
+            try:
+                evaluated_filters = list(ast.literal_eval(filters))
+                # If only one filter
+                if type(evaluated_filters[0]) == str:
+                    args['Filters'] = [ast.literal_eval(filters)]
+                else:
+                    args['Filters'] = evaluated_filters
+            except Exception as e:
+                return action_result.set_status(phantom.APP_ERROR, 'Error occured while parsing filter : {}'.format(e))
         if dry_run:
             args['DryRun'] = dry_run
         if network_acl_ids:
@@ -644,19 +663,26 @@ class AwsEc2Connector(BaseConnector):
         group_names = param.get('group_names')
         next_token = param.get('next_token')
         max_results = param.get('max_results')
+        self.debug_print('check max result')
+        self.debug_print(max_results)
+        self.debug_print(type(max_results))
+        self.debug_print('check max result')
 
         args = dict()
         if filters:
-            evaluated_filters = list(ast.literal_eval(filters))
-            # If only one filter
-            if type(evaluated_filters[0]) == str:
-                args['Filters'] = [ast.literal_eval(filters)]
-            else:
-                args['Filters'] = evaluated_filters
+            try:
+                evaluated_filters = list(ast.literal_eval(filters))
+                # If only one filter
+                if type(evaluated_filters[0]) == str:
+                    args['Filters'] = [ast.literal_eval(filters)]
+                else:
+                    args['Filters'] = evaluated_filters
+            except Exception as e:
+                return action_result.set_status(phantom.APP_ERROR, 'Error occured while parsing filter : {}'.format(e))
         if group_ids:
-            args['GroupIds'] = group_ids
+            args['GroupIds'] = group_ids.split(',')
         if group_names:
-            args['GroupNames'] = group_names
+            args['GroupNames'] = group_names.split(',')
         if dry_run:
             args['DryRun'] = dry_run
         if next_token:
@@ -689,13 +715,18 @@ class AwsEc2Connector(BaseConnector):
             'InstanceIds': [instance_id]
         }
 
+        self.debug_print('before boto call...{}'.format(args))
         # make rest call
         ret_val, response = self._make_boto_call(action_result, 'describe_instances', **args)
 
+        self.debug_print('after boto call...{}..{}'.format(response, ret_val))
         if (phantom.is_fail(ret_val)):
-            return action_result.get_status()
+            return (action_result.get_status(), None, None)
 
         original_groups = response.get('Reservations')[0].get('Instances')[0].get('SecurityGroups')
+        self.debug_print('hello123')
+        self.debug_print(original_groups)
+        self.debug_print('hello123END')
         group_list = [item.get('GroupId') for item in original_groups]
         network_interface_id = response.get('Reservations')[0].get('Instances')[0].get('NetworkInterfaces')[0].get('NetworkInterfaceId')
 
@@ -710,9 +741,9 @@ class AwsEc2Connector(BaseConnector):
 
         group_to_add = param['group_to_add']
         instance_id = param['instance_id']
-
+        self.debug_print('before security group call')
         ret_val, group_list, network_interface_id = self._security_group_helper(instance_id, action_result)
-
+        self.debug_print('after security group call{}...{}..{}'.format(group_list, network_interface_id, ret_val))
         if (phantom.is_fail(ret_val)):
             return action_result.get_status()
 
@@ -782,8 +813,11 @@ class AwsEc2Connector(BaseConnector):
             'Groups': group_list
         }
 
+        self.debug_print('before boto call')
+
         # make rest call
         ret_val, response = self._make_boto_call(action_result, 'modify_attribute', **args)
+        self.debug_print('after boto call')
 
         if (phantom.is_fail(ret_val)):
             return action_result.get_status()
@@ -853,15 +887,19 @@ class AwsEc2Connector(BaseConnector):
         network_interface_ids = param.get('network_interface_ids')
         next_token = param.get('next_token')
         max_results = param.get('max_results')
+        self.debug_print('list_network start')
 
         args = dict()
         if filters:
-            evaluated_filters = list(ast.literal_eval(filters))
-            # If only one filter
-            if type(evaluated_filters[0]) == str:
-                args['Filters'] = [ast.literal_eval(filters)]
-            else:
-                args['Filters'] = evaluated_filters
+            try:
+                evaluated_filters = list(ast.literal_eval(filters))
+                # If only one filter
+                if type(evaluated_filters[0]) == str:
+                    args['Filters'] = [ast.literal_eval(filters)]
+                else:
+                    args['Filters'] = evaluated_filters
+            except Exception as e:
+                return action_result.set_status(phantom.APP_ERROR, 'Error occured while parsing filter : {}'.format(e))
         if dry_run:
             args['DryRun'] = dry_run
         if network_interface_ids:
@@ -871,9 +909,10 @@ class AwsEc2Connector(BaseConnector):
         if max_results is not None:
             args['MaxResults'] = max_results
 
+        self.debug_print('before boto..{}'.format(args))
         # make rest call
         ret_val, response = self._make_boto_call(action_result, 'describe_network_interfaces', **args)
-
+        self.debug_print('after boto')
         if (phantom.is_fail(ret_val)):
             return action_result.get_status()
 
