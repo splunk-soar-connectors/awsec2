aed1400cb1c1bf6243bcbe077d4beb2de553a29f
Jay Patel <jay.patel@M06-JayP.local>
2019-04-12 17:19:35 +0530

    AWSEC2: Remaining bugs fixation and other output view related changes and adding custom output view

--- awsec2_connector.py
+++ awsec2_connector.py
@@ -1,4 +1,8 @@
-
+# File: awsec2_connector.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 # Phantom App imports
 import phantom.app as phantom
 from phantom.base_connector import BaseConnector
